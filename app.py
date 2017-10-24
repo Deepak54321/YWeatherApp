6453774bae3af52df6b66680caf5abae7db719f0
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-10-24 05:03:00 -0700

    Update app.py
    Api Updtate

--- app.py
+++ app.py
@@ -52,25 +52,14 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    baseurl = "http://m.ymslilabs.com/WebApiRequest/Price.txt"
+    yql_url = baseurl + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
+    
+    return data
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
